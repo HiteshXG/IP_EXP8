433a82bcec3644b97c67fa5dc6df852de7a45cb6
HiteshXG <140910237+HiteshXG@users.noreply.github.com>
2024-10-04 19:22:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const PORT = 3001; // Using 3001 as Create React App typically uses 3000
 
 const server = http.createServer(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://ip-exp-8-q0m9msm5x-mao-shi-chigamks-projects.vercel.app/read-file");
+  res.setHeader("Access-Control-Allow-Origin", "http://ip-exp-8-q0m9msm5x-mao-shi-chigamks-projects.vercel.app");
 
   if (req.url === "/read-file" && req.method === "GET") {
     try {
